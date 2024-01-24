8ce51447a78005f3e8afea520b66d41e4e67cc94
longjian_cool <longjian_cool@126.com>
2024-01-24 14:46:12 +0800

    init project

---
+++ src/main/java/com/springauth/springauthplus/SpringAuthPlusApplication.java
@@ -0,0 +1,13 @@
+package com.springauth.springauthplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthPlusApplication.class, args);
+	}
+
+}
